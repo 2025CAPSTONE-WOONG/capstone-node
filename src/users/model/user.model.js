c5a98b36185e5cf563b928e11b8cc2dfeb10a5ee
박준우 <97094633+Joonw00@users.noreply.github.com>
2025-05-12 22:08:43 +0900

    feature : 일반 로그인

--- src/users/model/user.model.js
+++ src/users/model/user.model.js
@@ -1,5 +1,6 @@
 const db = require('../../config/database');
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
 const findByEmail = async (email) => {
   const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
@@ -7,12 +8,17 @@ const findByEmail = async (email) => {
 };
 
 const create = async (userData) => {
-  const { email, name, profilePicture, provider } = userData;
+  const { email, name, profilePicture, provider, password } = userData;
   const id = uuidv4();
   
+  let hashedPassword = null;
+  if (password) {
+    hashedPassword = await bcrypt.hash(password, 10);
+  }
+  
   const result = await db.query(
-    'INSERT INTO users (id, nickname, major, goal, emotion) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-    [id, name, null, null, null]
+    'INSERT INTO users (id, email, password, nickname, major, goal, emotion) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+    [id, email, hashedPassword, name, null, null, null]
   );
   
   return result.rows[0];
@@ -32,9 +38,14 @@ const findById = async (id) => {
   return result.rows[0];
 };
 
+const verifyPassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
 module.exports = {
   findByEmail,
   create,
   update,
-  findById
+  findById,
+  verifyPassword
 }; 
